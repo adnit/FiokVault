54794a13f6e3382388e72708866719e2cd6e90f4
Kastriot Salihu <kastriot.salihu@student.uni-pr.edu>
2021-07-23 11:56:48 +0200

    rsa encrypt

---
+++ FiokVault/FiokVaultEncrypt.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace FiokVault
+{
+    class FiokVaultEncrypt
+    {
+        static string PublicKeyString = File.ReadAllText("..\\..\\..\\publickey.txt");
+
+        public byte[] encryptMessage(byte[] dataToEncrypt)
+        {
+            //Create a new instance of RSACryptoServiceProvider to generate
+            //public and private key data.
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSAParameters parameters = getPublicParameters(PublicKeyString);
+
+                //Pass the data to ENCRYPT, the public key information 
+                //(using RSACryptoServiceProvider.ExportParameters(false),
+                //and a boolean flag specifying no OAEP padding.
+                byte[] encryptedData = RSAEncrypt(dataToEncrypt, parameters, false);
+                return encryptedData;
+            }
+        }
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This only needs
+                    //toinclude the public key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Encrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        static public RSAParameters getPublicParameters(string keyString)
+        {
+            keyString = keyString.Substring(26, keyString.Length - 24 - 26);
+            //Asn1Object obj = Asn1Object.FromByteArray(Convert.FromBase64String(keyString));
+            Asn1Object obj = Asn1Object.FromByteArray(Convert.FromBase64String(keyString));
+            DerSequence publicKeySequence = (DerSequence)obj;
+
+            DerBitString encodedPublicKey = (DerBitString)publicKeySequence[1];
+            DerSequence publicKey = (DerSequence)Asn1Object.FromByteArray(encodedPublicKey.GetBytes());
+
+            DerInteger modulus = (DerInteger)publicKey[0];
+            DerInteger exponent = (DerInteger)publicKey[1];
+            RsaKeyParameters keyParameters = new RsaKeyParameters(false, modulus.PositiveValue, exponent.PositiveValue);
+            RSAParameters parameters = DotNetUtilities.ToRSAParameters(keyParameters);
+            return parameters;
+        }
+
+    }
+}
